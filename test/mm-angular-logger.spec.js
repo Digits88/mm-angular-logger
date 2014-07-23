50b9982e7c88bc812b05bfc665313f86e77796a3
martinmicunda <martinmicunda@hotmail.com>
2014-07-23 23:08:56 +0100

    chore(gulp): add bower install to pre serve task

--- test/mm-angular-logger.spec.js
+++ test/mm-angular-logger.spec.js
@@ -1,25 +1,5 @@
 'use strict';
 
-/* jasmine specs for filters go here */
-
-describe('filter', function() {
-
-    describe('checkmark', function() {
-
-
-        it('should be true....', inject(function() {
-            expect(true).toBe(true);
-        }));
-
-        it('should be false....', inject(function() {
-            expect(false).toBe(false);
-        }));
-    });
-});
-
-
-'use strict';
-
 describe('Module: mm.logger', function () {
 
     beforeEach(module('mm.logger'));
